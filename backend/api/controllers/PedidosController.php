d913aa2bbae86e71c7b342a11ddc1d6535f39306
Diana <diana.fp.gomes@gmail.pt>
2020-01-10 17:09:02 +0000

    Delete do controller default

--- backend/api/controllers/PedidosController.php
+++ backend/api/controllers/PedidosController.php
@@ -2,9 +2,38 @@
 
 namespace backend\api\controllers;
 
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\QueryParamAuth;
 use yii\rest\ActiveController;
 
 class PedidosController extends ActiveController
 {
     public $modelClass = 'backend\models\Pedido';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                [
+                    'class' => HttpBasicAuth::className(),
+                    'auth' =>  [$this, 'auth'],
+                ],
+                QueryParamAuth::className(),
+            ],
+        ];
+        return $behaviors;
+    }
+
+    public function auth($username, $password)
+    {
+        $user = \common\models\User::findByUsername($username);
+        if ($user && $user->validatePassword($password))
+        {
+            return $user;
+        }
+        return null;
+    }
 }
