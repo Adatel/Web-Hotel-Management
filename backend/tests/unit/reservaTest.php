b37d7d8cace19811010315730474c626f7416e14
Diana <diana.fp.gomes@gmail.pt>
2020-01-15 01:15:22 +0000

    Teste unitário - Reserva

--- backend/tests/unit/reservaTest.php
+++ backend/tests/unit/reservaTest.php
@@ -1,5 +1,7 @@
 <?php namespace backend\tests;
 
+use common\models\Reserva;
+
 class reservaTest extends \Codeception\Test\Unit
 {
     /**
@@ -18,6 +20,50 @@ class reservaTest extends \Codeception\Test\Unit
     // tests
     public function testValidation()
     {
+        $reserva = new Reserva();
 
+
+        // <--------------------------------------------- NUM QUARTOS --------------------------------------------->
+        $reserva->setNumQuarto(null);
+        $this->assertFalse($reserva->validate(['num_quartos']));
+
+        $reserva->setNumQuarto("Num Quarto teste");
+        $this->assertFalse($reserva->validate(['num_quartos']));
+
+        $reserva->setNumQuarto(3);
+        $this->assertTrue($reserva->validate(['num_quartos']));
+
+
+        // <--------------------------------------------- NUM PESSOAS --------------------------------------------->
+        $reserva->setNumPessoas(null);
+        $this->assertFalse($reserva->validate(['num_pessoas']));
+
+        $reserva->setNumPessoas("Num Pessoas teste");
+        $this->assertFalse($reserva->validate(['num_pessoas']));
+
+        $reserva->setNumPessoas(3);
+        $this->assertTrue($reserva->validate(['num_pessoas']));
+
+
+        // <--------------------------------------------- DATA ENTRADA --------------------------------------------->
+        $reserva->setDataEntrada(null);
+        $this->assertFalse($reserva->validate(['data_entrada']));
+
+        $reserva->setDataEntrada(2020-01-15);
+        $this->assertTrue($reserva->validate(['data_entrada']));
+
+        $reserva->setDataEntrada("2020-01-15");
+        $this->assertTrue($reserva->validate(['data_entrada']));
+
+
+        // <--------------------------------------------- DATA SAIDA --------------------------------------------->
+        $reserva->setDataSaida(null);
+        $this->assertFalse($reserva->validate(['data_saida']));
+
+        $reserva->setDataSaida(2020-01-15);
+        $this->assertTrue($reserva->validate(['data_saida']));
+
+        $reserva->setDataSaida("2020-01-15");
+        $this->assertTrue($reserva->validate(['data_saida']));
     }
 }
