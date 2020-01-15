5bee5804d72dad5e73afa5ba1149781b859dfdc2
Diana <diana.fp.gomes@gmail.pt>
2020-01-15 02:44:50 +0000

    Test Acceptance

---
+++ frontend/tests/acceptance/acceptanceTest.php
@@ -0,0 +1,23 @@
+<?php namespace frontend\tests;
+
+class acceptanceTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \frontend\tests\AcceptanceTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
